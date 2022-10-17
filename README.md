2edf86d1b247e1a458d52cc12f9646918f96c861
Vincent Lu <vincentlu@HaleydeMacBook-Pro.local>
2022-10-17 22:46:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+### Init
+
+```sh
+yarn
+```
+
+### Compile contract to ABIs
+```sh
+yarn compile
+```
+
+### Deploy subgraphs for exmaple
+
+```sh
+yarn codegen
+
+# local graph service
+yarn create-local
+
+yarn deploy-local
+
+yarn remove-local
+
+# Thegraph Hosted-Service
+graph auth --product hosted-service ${AccessToken}
+
+yarn build-example
+
+yarn deploy-example-host
+
+```
+
+
+
+
+
+
---
+++ handlers/example/handler.ts
@@ -0,0 +1,30 @@
+import { NewLPMinted } from '../../generated/MetaDefender/LiquidityCertificate';
+import { LiquidityCertificate } from '../../generated/schema';
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+
+export function handleNewLPMinted(event: NewLPMinted): void {
+    updateEntity(event.params.certificateId, event.params.timestamp, event.params.expiryTime, event.params.liquidity, event.params.rewardDebt, event.params.shadowDebt)
+    return
+}
+
+function updateEntity(certificateId: BigInt, timestamp: BigInt, expiryTime: BigInt, liquidity:BigInt, rewardDebt: BigInt, shadowDebt: BigInt): void {
+    let entity = LiquidityCertificate.load(certificateId.toString())
+    if (entity == null) {
+        entity = new LiquidityCertificate(certificateId.toString())
+        entity.timestamp = timestamp;
+        entity.expiryTime = expiryTime;
+        entity.liquidity = liquidity;
+        entity.rewardDebt = rewardDebt;
+        entity.shadowDebt = shadowDebt;
+        entity.save()
+        return
+    } else {
+        entity.timestamp = timestamp;
+        entity.expiryTime = expiryTime;
+        entity.liquidity = liquidity;
+        entity.rewardDebt = rewardDebt;
+        entity.shadowDebt = shadowDebt;
+        entity.save()
+        return
+    }
+}
