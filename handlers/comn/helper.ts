34a52c0527431cbe3b2785551e8ed46d11cd181a
Vincent Lu <vincentlu@M1_Mac.lan>
2022-10-27 16:31:39 +0800

    impl: certificateId and medalId

--- handlers/comn/helper.ts
+++ handlers/comn/helper.ts
@@ -1,28 +1,28 @@
-import { BigDecimal, BigInt, log } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt, log } from '@graphprotocol/graph-ts';
 
-export let ZERO_BI = BigInt.fromI32(0)
-export let ONE_BI = BigInt.fromI32(1)
-export let ZERO_BD = BigDecimal.fromString('0')
-export let ONE_BD = BigDecimal.fromString('1')
-export let BI_18 = BigInt.fromI32(18)
-export let BD_18 = BigDecimal.fromString('1000000000000000000')
+export const ZERO_BI = BigInt.fromI32(0);
+export const ONE_BI = BigInt.fromI32(1);
+export const ZERO_BD = BigDecimal.fromString('0');
+export const ONE_BD = BigDecimal.fromString('1');
+export const BI_18 = BigInt.fromI32(18);
+export const BD_18 = BigDecimal.fromString('1000000000000000000');
 
 export function convertTokenToDecimal(
-    tokenAmount: BigInt,
-    exchangeDecimals: BigInt
+    tokenAmount: bigint,
+    exchangeDecimals: bigint,
 ): BigDecimal {
     if (exchangeDecimals == ZERO_BI) {
-        return tokenAmount.toBigDecimal()
+        return tokenAmount.toBigDecimal();
     }
     return tokenAmount
         .toBigDecimal()
-        .div(exponentToBigDecimal(exchangeDecimals))
+        .div(exponentToBigDecimal(exchangeDecimals));
 }
 
-export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
-    let bd = BigDecimal.fromString('1')
-    for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
-        bd = bd.times(BigDecimal.fromString('10'))
+export function exponentToBigDecimal(decimals: bigint): BigDecimal {
+    let bd = BigDecimal.fromString('1');
+    for (let i = ZERO_BI; i.lt(decimals as bigint); i = i.plus(ONE_BI)) {
+        bd = bd.times(BigDecimal.fromString('10'));
     }
-    return bd
+    return bd;
 }
--- handlers/handler.ts
+++ handlers/handler.ts
@@ -1,47 +1,100 @@
-import { NewLPMinted } from '../generated/MetaDefender/LiquidityCertificate';
-import {LiquidityCertificate, LiquidityMedal} from '../generated/schema';
-import { Address, BigInt } from '@graphprotocol/graph-ts'
-import {NewMedalMinted} from "../generated/MetaDefender/LiquidityMedal";
+import { LiquidityCertificate, LiquidityMedal } from '../generated/schema';
+import { NewMedalMinted } from '../generated/LiquidityMedal/LiquidityMedal';
+import {
+    NewLPMinted,
+    LPBurned,
+} from '../generated/LiquidityCertificate/LiquidityCertificate';
+import * as graphTypes from '@graphprotocol/graph-ts';
+import { store } from '@graphprotocol/graph-ts';
 
 export function handleNewLPMinted(event: NewLPMinted): void {
-    updateLiquidityCertificate(event.params.owner, event.params.certificateId, event.params.timestamp, event.params.expiryTime, event.params.liquidity, event.params.rewardDebt, event.params.shadowDebt)
-    return
+    updateLiquidityCertificate(
+        event.params.owner,
+        event.params.certificateId,
+        event.params.timestamp,
+        event.params.expiryTime,
+        event.params.liquidity,
+        event.params.rewardDebt,
+        event.params.shadowDebt,
+    );
+    return;
 }
 
-export function handleNewMedalMinted(event:NewMedalMinted): void {
-    updateLiquidityMedal(event.params.medalId, event.params.enteredAT , event.params.exitedAt, event.params.liquidity, event.params.reserve, event.params.shadowDebt, event.params.marketShadow)
-    return
+export function handleLPBurned(event: LPBurned): void {
+    deleteLiquidityCertificate(event.params.certificateId);
+    return;
 }
 
-function updateLiquidityCertificate(owner:String, certificateId: BigInt, timestamp: BigInt, expiryTime: BigInt, liquidity:BigInt, rewardDebt: BigInt, shadowDebt: BigInt): void {
-    let entity = LiquidityCertificate.load(certificateId.toString())
-    if (entity == null) {
-        entity = new LiquidityCertificate(certificateId.toString())
+function deleteLiquidityCertificate(certificateId: graphTypes.BigInt): void {
+    const entity = LiquidityCertificate.load(certificateId.toString());
+    if (entity != null) {
+        store.remove('LiquidityCertificate', certificateId.toString());
     }
-    entity.owner = owner;
+    return;
+}
+
+export function handleNewMedalMinted(event: NewMedalMinted): void {
+    updateLiquidityMedal(
+        event.params.owner,
+        event.params.medalId,
+        event.params.enteredAT,
+        event.params.exitedAt,
+        event.params.liquidity,
+        event.params.reserve,
+        event.params.shadowDebt,
+        event.params.marketShadow,
+    );
+    return;
+}
+
+function updateLiquidityCertificate(
+    owner: graphTypes.Address,
+    certificateId: graphTypes.BigInt,
+    timestamp: graphTypes.BigInt,
+    expiryTime: graphTypes.BigInt,
+    liquidity: graphTypes.BigInt,
+    rewardDebt: graphTypes.BigInt,
+    shadowDebt: graphTypes.BigInt,
+): void {
+    let entity = LiquidityCertificate.load(certificateId.toString());
+    if (entity == null) {
+        // in this scenario, the id of the entity is the same as the id of the certificate
+        entity = new LiquidityCertificate(certificateId.toString());
+    }
+    entity.owner = owner.toHexString();
     entity.certificateId = certificateId;
     entity.timestamp = timestamp;
     entity.expiryTime = expiryTime;
     entity.liquidity = liquidity;
     entity.rewardDebt = rewardDebt;
     entity.shadowDebt = shadowDebt;
-    entity.save()
-    return
+    entity.save();
+    return;
 }
 
-
-function updateLiquidityMedal(medalId: BigInt, enteredAt:BigInt, exitedAt: BigInt, liquidity: BigInt, reserve:BigInt, shadowDebt: BigInt, marketShadow:BigInt): void {
-    let entity = LiquidityMedal.load(medalId.toString())
+function updateLiquidityMedal(
+    owner: graphTypes.Address,
+    medalId: graphTypes.BigInt,
+    enteredAt: graphTypes.BigInt,
+    exitedAt: graphTypes.BigInt,
+    liquidity: graphTypes.BigInt,
+    reserve: graphTypes.BigInt,
+    shadowDebt: graphTypes.BigInt,
+    marketShadow: graphTypes.BigInt,
+): void {
+    let entity = LiquidityMedal.load(medalId.toString());
     if (entity == null) {
-        entity = new LiquidityMedal(medalId.toString())
+        // in this scenario, the id of the entity is the same as the id of the medal
+        entity = new LiquidityMedal(medalId.toString());
     }
+    entity.owner = owner.toHexString();
     entity.medalId = medalId;
     entity.enteredAt = enteredAt;
-    entity.exitedAt= exitedAt;
+    entity.exitedAt = exitedAt;
     entity.liquidity = liquidity;
     entity.reserve = reserve;
     entity.shadowDebt = shadowDebt;
     entity.marketShadow = marketShadow;
-    entity.save()
-    return
+    entity.save();
+    return;
 }
