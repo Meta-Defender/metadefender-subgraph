e92178b9b5b802a6353718f80a88085708225f95
Vincent Lu <vincentlu@M1_Mac.lan>
2022-10-27 11:16:12 +0800

    first commit

--- handlers/handler.ts
+++ handlers/handler.ts
@@ -1,30 +1,47 @@
 import { NewLPMinted } from '../generated/MetaDefender/LiquidityCertificate';
-import { LiquidityCertificate } from '../generated/schema';
+import {LiquidityCertificate, LiquidityMedal} from '../generated/schema';
 import { Address, BigInt } from '@graphprotocol/graph-ts'
+import {NewMedalMinted} from "../generated/MetaDefender/LiquidityMedal";
 
 export function handleNewLPMinted(event: NewLPMinted): void {
-    updateEntity(event.params.certificateId, event.params.timestamp, event.params.expiryTime, event.params.liquidity, event.params.rewardDebt, event.params.shadowDebt)
+    updateLiquidityCertificate(event.params.owner, event.params.certificateId, event.params.timestamp, event.params.expiryTime, event.params.liquidity, event.params.rewardDebt, event.params.shadowDebt)
     return
 }
 
-function updateEntity(certificateId: BigInt, timestamp: BigInt, expiryTime: BigInt, liquidity:BigInt, rewardDebt: BigInt, shadowDebt: BigInt): void {
+export function handleNewMedalMinted(event:NewMedalMinted): void {
+    updateLiquidityMedal(event.params.medalId, event.params.enteredAT , event.params.exitedAt, event.params.liquidity, event.params.reserve, event.params.shadowDebt, event.params.marketShadow)
+    return
+}
+
+function updateLiquidityCertificate(owner:String, certificateId: BigInt, timestamp: BigInt, expiryTime: BigInt, liquidity:BigInt, rewardDebt: BigInt, shadowDebt: BigInt): void {
     let entity = LiquidityCertificate.load(certificateId.toString())
     if (entity == null) {
         entity = new LiquidityCertificate(certificateId.toString())
-        entity.timestamp = timestamp;
-        entity.expiryTime = expiryTime;
-        entity.liquidity = liquidity;
-        entity.rewardDebt = rewardDebt;
-        entity.shadowDebt = shadowDebt;
-        entity.save()
-        return
-    } else {
-        entity.timestamp = timestamp;
-        entity.expiryTime = expiryTime;
-        entity.liquidity = liquidity;
-        entity.rewardDebt = rewardDebt;
-        entity.shadowDebt = shadowDebt;
-        entity.save()
-        return
     }
+    entity.owner = owner;
+    entity.certificateId = certificateId;
+    entity.timestamp = timestamp;
+    entity.expiryTime = expiryTime;
+    entity.liquidity = liquidity;
+    entity.rewardDebt = rewardDebt;
+    entity.shadowDebt = shadowDebt;
+    entity.save()
+    return
+}
+
+
+function updateLiquidityMedal(medalId: BigInt, enteredAt:BigInt, exitedAt: BigInt, liquidity: BigInt, reserve:BigInt, shadowDebt: BigInt, marketShadow:BigInt): void {
+    let entity = LiquidityMedal.load(medalId.toString())
+    if (entity == null) {
+        entity = new LiquidityMedal(medalId.toString())
+    }
+    entity.medalId = medalId;
+    entity.enteredAt = enteredAt;
+    entity.exitedAt= exitedAt;
+    entity.liquidity = liquidity;
+    entity.reserve = reserve;
+    entity.shadowDebt = shadowDebt;
+    entity.marketShadow = marketShadow;
+    entity.save()
+    return
 }
